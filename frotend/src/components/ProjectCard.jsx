a5e97271b30185b742d6baebb4cbd0122a45d624
GuillermoEAC <guillermochina12@gmail.com>
2026-02-08 01:42:03 -0700

    ahora sera angular alv

--- frotend/src/components/ProjectCard.jsx
+++
@@ -1,3 +0,0 @@
-// \frotend\src\components\ProjectCard.jsx
-// Para mostrar la info de cada proyecto
-// Cuchao 🌩️
--- frotend/src/global/ContextoProyecto.jsx
+++
@@ -1,8 +0,0 @@
-// import { Form } from "lucide-react";
-// import React, { crearContexto, useState, useContext } from "react";
-
-// const ContextoProyecto = crearContexto();
-
-// export const ProveedorProyecto = ({ children }) => {
-//   const [proyectoActual, setProyectoActual] = useState(null);
-// };
