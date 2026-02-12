0e91d5564e928590af5e594f04dea5279b299d60
GuillermoEAC <guillermochina12@gmail.com>
2026-02-12 00:01:09 -0700

    bueno peor estaba

--- project-manager/src/app/shared/layout/layout-principal.ts
+++ project-manager/src/app/shared/layout/layout-principal.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-layout-principal',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, MatIconModule],
   templateUrl: './layout-principal.html',
   styleUrl: './layout-principal.css',
 })
-export class LayoutPrincipal {}
+export class LayoutPrincipal {
+  isCollapsed = false;
+
+  alternarSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
